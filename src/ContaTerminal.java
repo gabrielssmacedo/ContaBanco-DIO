bc9042143638deabe8d9f9ba6c1d8be18490a7e0
gabrielssmacedo <gabrielmacedo317@gmail.com>
2025-06-19 18:29:08 -0300

    feat: add user log on

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,6 +1,30 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
 
+
     public static void main(String[] args) {
-        System.out.println("Ola");
+        Locale.setDefault(Locale.US);
+        int numero;
+        String agencia, nomeCliente;
+        double saldo;
+        Scanner sc = new Scanner(System.in);
+
+        numero = (int)(Math.random()*10000);
+
+        System.out.printf("Bem vindo Usuário %d!\n", numero);
+        System.out.print("Por favor, digite sua Agencia: ");
+        agencia = sc.next();
+        System.out.print("Digite seu nome: ");
+        sc.nextLine();
+        nomeCliente = sc.nextLine();
+        System.out.print("Insira um valor de depósito: ");
+
+        saldo = sc.nextDouble();
+
+        System.out.printf("\nOlá %s, obrigado por criar uma conta em nosso banco ", nomeCliente);
+        System.out.printf("sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque", agencia, numero, saldo);
+
     }
 }
